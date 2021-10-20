adb6239e3452a2855a7c84712c9eb5c6c796b2af
Michal Schielmann <michalschielmann@gmail.com>
2021-10-20 21:24:04 +0200

    refactor, extracted local variables for max size

--- recently-used-list/src/test/java/pl/mschielmann/tddkata/recentlyusedlist/RecentlyUsedListTest.java
+++ recently-used-list/src/test/java/pl/mschielmann/tddkata/recentlyusedlist/RecentlyUsedListTest.java
@@ -6,10 +6,12 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class RecentlyUsedListTest
 {
+
     @Test
     public void when_no_elements_added_list_is_empty()
     {
-        RecentlyUsedList recentlyUsedList = new RecentlyUsedList(1);
+        int maxSize = 1;
+        RecentlyUsedList recentlyUsedList = new RecentlyUsedList(maxSize);
 
         assertEquals(0, recentlyUsedList.size());
     }
@@ -17,39 +19,50 @@ public class RecentlyUsedListTest
     @Test
     public void when_one_element_added_size_is_1()
     {
-        RecentlyUsedList recentlyUsedList = new RecentlyUsedList(1);
+        int maxSize = 1;
+        RecentlyUsedList recentlyUsedList = new RecentlyUsedList(maxSize);
+
         recentlyUsedList.add("ElementOne");
-        assertEquals(1, recentlyUsedList.size());
+
+        assertEquals(maxSize, recentlyUsedList.size());
     }
 
     @Test
     public void when_one_element_added_it_is_first_on_the_list()
     {
-        RecentlyUsedList recentlyUsedList = new RecentlyUsedList(1);
+        int maxSize = 1;
+        RecentlyUsedList recentlyUsedList = new RecentlyUsedList(maxSize);
+
         recentlyUsedList.add("ElementOne");
+
         assertEquals("ElementOne", recentlyUsedList.getElement(0));
     }
 
     @Test
     public void when_one_element_added_and_removed_then_list_is_empty()
     {
-        RecentlyUsedList recentlyUsedList = new RecentlyUsedList(1);
+        int maxSize = 1;
+        RecentlyUsedList recentlyUsedList = new RecentlyUsedList(maxSize);
+
         recentlyUsedList.add("ElementOne");
         recentlyUsedList.remove(0);
+
         assertEquals(0, recentlyUsedList.size());
     }
 
     @Test
     public void when_max_number_of_elements_added_then_all_are_in_the_list_in_opposite_order()
     {
-        RecentlyUsedList recentlyUsedList = new RecentlyUsedList(5);
+        int maxSize = 5;
+        RecentlyUsedList recentlyUsedList = new RecentlyUsedList(maxSize);
+
         recentlyUsedList.add("Element1");
         recentlyUsedList.add("Element2");
         recentlyUsedList.add("Element3");
         recentlyUsedList.add("Element4");
         recentlyUsedList.add("Element5");
 
-        assertEquals(5, recentlyUsedList.size());
+        assertEquals(maxSize, recentlyUsedList.size());
         assertEquals("Element5", recentlyUsedList.getElement(0));
         assertEquals("Element4", recentlyUsedList.getElement(1));
         assertEquals("Element3", recentlyUsedList.getElement(2));
@@ -60,7 +73,9 @@ public class RecentlyUsedListTest
     @Test
     public void when_more_than_max_number_of_elements_added_then_eldest_gets_removed()
     {
-        RecentlyUsedList recentlyUsedList = new RecentlyUsedList(5);
+        int maxSize = 5;
+        RecentlyUsedList recentlyUsedList = new RecentlyUsedList(maxSize);
+
         recentlyUsedList.add("Element1");
         recentlyUsedList.add("Element2");
         recentlyUsedList.add("Element3");
@@ -69,11 +84,12 @@ public class RecentlyUsedListTest
         recentlyUsedList.add("Element6");
         recentlyUsedList.add("Element7");
 
-        assertEquals(5, recentlyUsedList.size());
+        assertEquals(maxSize, recentlyUsedList.size());
         assertEquals("Element7", recentlyUsedList.getElement(0));
         assertEquals("Element6", recentlyUsedList.getElement(1));
         assertEquals("Element5", recentlyUsedList.getElement(2));
         assertEquals("Element4", recentlyUsedList.getElement(3));
         assertEquals("Element3", recentlyUsedList.getElement(4));
     }
+
 }
