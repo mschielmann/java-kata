2213e42600523d8a0c2fedcc58ade90405072e58
Michal Schielmann <michalschielmann@gmail.com>
2021-10-21 12:30:23 +0200

    fixed pom for jupiter deps

--- simple-hashmap/src/main/java/pl/mschielmann/tddkata/simplehashmap/SimpleHashMap.java
+++ simple-hashmap/src/main/java/pl/mschielmann/tddkata/simplehashmap/SimpleHashMap.java
@@ -3,15 +3,8 @@ package pl.mschielmann.tddkata.simplehashmap;
 class SimpleHashMap
 {
 
-    private int size = 0;
-
     int size()
     {
-        return 1;
-    }
-
-    void put(String key, String value)
-    {
-        size++;
+        return 0;
     }
 }
