ed593ac82d7f00ba65d01df3e33277f8e13a82c3
Michal Schielmann <michalschielmann@gmail.com>
2021-10-26 12:24:24 +0200

    first sudoku solver test

--- sudoku-solver/src/main/java/pl/mschielmann/tddkata/sudokusolver/SudokuPuzzle.java
+++ sudoku-solver/src/main/java/pl/mschielmann/tddkata/sudokusolver/SudokuPuzzle.java
@@ -2,19 +2,23 @@ package pl.mschielmann.tddkata.sudokusolver;
 
 class SudokuPuzzle
 {
+    private final int dimension;
+    private final String input;
+
     public SudokuPuzzle(int dimension, String input)
     {
-
+        this.dimension = dimension;
+        this.input = input;
     }
 
     boolean isSolved()
     {
-        return true;
+        return input.equals("1");
     }
 
 
     String display()
     {
-        return "1";
+        return input;
     }
 }
--- sudoku-solver/src/test/java/pl/mschielmann/tddkata/sudokusolver/SudokuSolverTest.java
+++ sudoku-solver/src/test/java/pl/mschielmann/tddkata/sudokusolver/SudokuSolverTest.java
@@ -3,6 +3,7 @@ package pl.mschielmann.tddkata.sudokusolver;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class SudokuSolverTest
@@ -15,6 +16,16 @@ class SudokuSolverTest
 
         assertTrue(puzzle.isSolved());
         assertEquals("1", puzzle.display());
+    }
+
+    @Test
+    public void given_1_dimension_puzzle_with_empty_input_displays_empty_puzzle()
+    {
+        String input = " ";
+        SudokuPuzzle puzzle = new SudokuPuzzle(1, input);
+
+        assertFalse(puzzle.isSolved());
+        assertEquals(" ", puzzle.display());
     }
 
 }
